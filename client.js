3b6073108aeabd00a3a27eed41e73fe7b26a2c52
Christoph Dorn <christoph@christophdorn.com>
2015-01-15 21:47:13 -0800

    wip

--- client.js
+++ client.js
@@ -1,14 +1,26 @@
 
 const JQUERY = require("jquery/dist/jquery");
-
-console.log("JQUERY", JQUERY);
-
 const TURN = require("turn/turn");
 
 
 exports.main = function () {
 
-	console.log("TURN", TURN);
+	var flipbookNode = JQUERY('<div id="flipbook"></div>').appendTo("BODY");
+	flipbookNode.html([
+		'<div class="hard"> Turn.js </div>'
+		'<div class="hard"></div>'
+		'<div> Page 1 </div>'
+		'<div> Page 2 </div>'
+		'<div> Page 3 </div>'
+		'<div> Page 4 </div>'
+		'<div class="hard"></div>'
+		'<div class="hard"></div>'
+	].join("\n"));
+
+	JQUERY("#flipbook").turn({
+		width: 400,
+		height: 300,
+		autoCenter: true
+	});
 
 }
-
